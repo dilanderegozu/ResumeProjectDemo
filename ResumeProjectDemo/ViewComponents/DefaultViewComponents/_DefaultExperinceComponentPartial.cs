cbdabbad20daad80c527089fdd70866723dc5079
Dilan <dilanderegizu@icloud.com>
2026-04-11 14:42:21 +0300

    fixed

---
+++ ResumeProjectDemo/ViewComponents/DefaultViewComponents/_DefaultExperinceComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeProjectDemo.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultExperinceComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
