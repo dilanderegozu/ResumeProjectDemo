b8d54d039591ad9d851513049b4e31047b0706fb
Dilan <dilanderegizu@icloud.com>
2026-04-16 19:05:16 +0300

    fixed

--- ResumeProjectDemo/Entities/Experience.cs
+++ ResumeProjectDemo/Entities/Experience.cs
@@ -1,10 +1,18 @@
-﻿namespace ResumeProjectDemo.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ResumeProjectDemo.Entities
 {
     public class Experience
     {
         public int ExperienceId { get; set; }
+
+        [Required(ErrorMessage = "Şirket adı ve unvan zorunludur.")]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Çalışma tarihi zorunludur.")]
         public string WorkDate { get; set; }
+
+        [Required(ErrorMessage = "Açıklama zorunludur.")]
         public string Description { get; set; }
     }
 }
